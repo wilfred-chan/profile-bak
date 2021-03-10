3e41c81d760fbcbdc3c0af42f2dae6f2cd3c6395
Wilfred Chan <69495364+wilfred-chan@users.noreply.github.com>
2021-03-10 14:53:27 +0800

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5,3 +5,5 @@ echo Y | sudo apt-get install git
 sudo apt install zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+ssh-keygen -t ed25519 -C "69495364+wilfred-chan@users.noreply.github.com"
