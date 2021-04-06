9e9df167b3fe00d6674dc6245531b59eabd83f97
Wilfred-Chan <69495364+wilfred-chan@users.noreply.github.com>
2021-04-06 08:12:19 +0800

    Update ubuntu setup scripts

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,9 +1,15 @@
 sudo apt-get update
+sudo apt-get upgrade
+echo Y | sudo apt-get remove vim.tiny
 echo Y | sudo apt-get install vim
 echo Y | sudo apt-get install git
 
 sudo apt install zsh
 chsh -s $(which zsh)
+# relogin to use zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-ssh-keygen -t ed25519 -C "69495364+wilfred-chan@users.noreply.github.com"
+# git config
+ssh-keygen -t ed25519 -C "YOUR_EMAIL_HERE"
+git config --global user.name "Wilfred-Chan"
+git config --global user.email "69495364+wilfred-chan@users.noreply.github.com"
