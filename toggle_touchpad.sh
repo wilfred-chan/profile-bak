82e02cf1eca34ced9128365ec485ffefe5fff52d
Wilfred-Chan <69495364+wilfred-chan@users.noreply.github.com>
2021-04-30 21:53:21 +0800

    Edit comments

--- toggle_touchpad.sh
+++ toggle_touchpad.sh
@@ -2,11 +2,10 @@
 # Source: https://askubuntu.com/a/863750 by WinEunuuchs2Unix
 # Customized by Wilfred Chan
 
-# NAME: toggle-touchpad
-# PATH: /home/$USERNAME/Scripts/toggle_touchpad.sh (This PATH is used in Ubuntu setting)
+# NAME: toggle-touchpad.sh
+# PATH: /home/$USERNAME/Scripts/toggle_touchpad.sh
+# (This PATH is used in Ubuntu setting, can be changed)
 # DESC: Update pulseaudio output device when HDMI TV plugged / unplugged
-# DATE: Created Dec 23, 2016.
-# NOTE: Written for AU question: http://askubuntu.com/questions/863746/keyboard-shortcut-to-disable-the-laptop-touchpad/863750?noredirect=1#comment1333958_863750
 
 
 # Use device number matching touchpad, in this case 16
